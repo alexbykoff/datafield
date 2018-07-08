c67991d2d94e4b7473e7b5984cef7b6d0c7f9527
Alex Bykov <spbeat@mail.ru>
2018-07-08 14:33:00 +0300

    init

---
+++ data.js
@@ -0,0 +1,382 @@
+module.exports = [
+  {
+    "_id": "5b40d2322746d9b161ad6445",
+    "index": 0,
+    "guid": "4c097921-0433-47af-affe-c8db55364829",
+    "isActive": false,
+    "balance": "$1,791.39",
+    "age": 21,
+    "name": {
+      "first": "Gretchen",
+      "last": "Myers"
+    },
+    "company": "STELAECOR",
+    "email": "gretchen.myers@stelaecor.info",
+    "phone": "+1 (886) 522-2201",
+    "address": "618 Halleck Street, Strykersville, Northern Mariana Islands, 6391",
+    "registered": "Thursday, January 19, 2017 6:26 AM",
+    "tags": [
+      "ea",
+      "occaecat",
+      "cillum"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Jimmie Hensley"
+      },
+      {
+        "id": 1,
+        "name": "Nash Young"
+      },
+      {
+        "id": 2,
+        "name": "Bridges Wooten"
+      }
+    ],
+    "greeting": "Hello, Gretchen! You have 7 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "5b40d2323b0f9ada9f1dadfc",
+    "index": 1,
+    "guid": "bf28c93d-bfeb-4f7d-8066-af66eefc3bec",
+    "isActive": false,
+    "balance": "$3,902.94",
+    "age": 39,
+    "name": {
+      "first": "Nettie",
+      "last": "Petty"
+    },
+    "company": "NETUR",
+    "email": "nettie.petty@netur.com",
+    "phone": "+1 (952) 420-2077",
+    "address": "332 Bergen Place, Glenville, Indiana, 4769",
+    "registered": "Saturday, July 7, 2018 5:03 AM",
+    "tags": [
+      "magna",
+      "fugiat",
+      "nostrud"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Deborah Burns"
+      },
+      {
+        "id": 1,
+        "name": "Hampton Miranda"
+      },
+      {
+        "id": 2,
+        "name": "Hale Mooney"
+      }
+    ],
+    "greeting": "Hello, Nettie! You have 5 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "5b40d2326f14343fb69540a1",
+    "index": 2,
+    "guid": "95004c03-f289-4bd2-a48f-3dbbc2f02e8b",
+    "isActive": true,
+    "balance": "$2,847.22",
+    "age": 29,
+    "name": {
+      "first": "Hyde",
+      "last": "Wiley"
+    },
+    "company": "VOLAX",
+    "email": "hyde.wiley@volax.co.uk",
+    "phone": "+1 (929) 516-3881",
+    "address": "372 Pierrepont Place, Riegelwood, Palau, 1265",
+    "registered": "Sunday, May 15, 2016 1:43 AM",
+    "tags": [
+      "dolore",
+      "aute",
+      "reprehenderit"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Perez Wood"
+      },
+      {
+        "id": 1,
+        "name": "Angela Nieves"
+      },
+      {
+        "id": 2,
+        "name": "Mable Sparks"
+      }
+    ],
+    "greeting": "Hello, Hyde! You have 8 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "5b40d2327359903c272fdbd9",
+    "index": 3,
+    "guid": "be55eda1-9c74-4235-972d-184c6e8c5025",
+    "isActive": true,
+    "balance": "$1,835.01",
+    "age": 29,
+    "name": {
+      "first": "Hernandez",
+      "last": "Stevenson"
+    },
+    "company": "COWTOWN",
+    "email": "hernandez.stevenson@cowtown.net",
+    "phone": "+1 (863) 429-2206",
+    "address": "412 Perry Place, Ada, Alabama, 2325",
+    "registered": "Sunday, November 1, 2015 8:39 PM",
+    "tags": [
+      "aliqua",
+      "sit",
+      "fugiat"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Patti Chang"
+      },
+      {
+        "id": 1,
+        "name": "Reese Stewart"
+      },
+      {
+        "id": 2,
+        "name": "Ursula Morin"
+      }
+    ],
+    "greeting": "Hello, Hernandez! You have 9 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "5b40d2321b1e69965b78f947",
+    "index": 4,
+    "guid": "1365a187-bc4d-4b60-8083-45650db0b458",
+    "isActive": false,
+    "balance": "$2,207.76",
+    "age": 25,
+    "name": {
+      "first": "Sharlene",
+      "last": "Ward"
+    },
+    "company": "CORPULSE",
+    "email": "sharlene.ward@corpulse.biz",
+    "phone": "+1 (845) 518-3920",
+    "address": "508 Vermont Street, Brownlee, Washington, 5540",
+    "registered": "Saturday, October 3, 2015 2:05 AM",
+    "tags": [
+      "est",
+      "fugiat",
+      "nulla"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Conway Townsend"
+      },
+      {
+        "id": 1,
+        "name": "Barrera Gay"
+      },
+      {
+        "id": 2,
+        "name": "Nola Riggs"
+      }
+    ],
+    "greeting": "Hello, Sharlene! You have 10 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "5b40d23250817c3b56ed797d",
+    "index": 5,
+    "guid": "fe34e322-3332-43e4-bb6a-5bf2b03c87ef",
+    "isActive": false,
+    "balance": "$1,948.71",
+    "age": 26,
+    "name": {
+      "first": "Jeannie",
+      "last": "Jefferson"
+    },
+    "company": "DOGTOWN",
+    "email": "jeannie.jefferson@dogtown.ca",
+    "phone": "+1 (802) 479-3352",
+    "address": "539 Senator Street, Garberville, New Hampshire, 7924",
+    "registered": "Tuesday, April 7, 2015 8:21 AM",
+    "tags": [
+      "est",
+      "aliquip",
+      "amet"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Sheena Watson"
+      },
+      {
+        "id": 1,
+        "name": "Ward Finch"
+      },
+      {
+        "id": 2,
+        "name": "Carey Price"
+      }
+    ],
+    "greeting": "Hello, Jeannie! You have 5 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "5b40d23249955e477bac87ed",
+    "index": 6,
+    "guid": "0495f1a2-54da-48cc-8070-5d1c299c3847",
+    "isActive": false,
+    "balance": "$2,237.81",
+    "age": 40,
+    "name": {
+      "first": "Morgan",
+      "last": "Kent"
+    },
+    "company": "DIGIRANG",
+    "email": "morgan.kent@digirang.name",
+    "phone": "+1 (906) 451-3102",
+    "address": "941 Delmonico Place, Fresno, District Of Columbia, 4498",
+    "registered": "Wednesday, April 25, 2018 6:48 AM",
+    "tags": [
+      "est",
+      "consequat",
+      "enim"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Marva Harmon"
+      },
+      {
+        "id": 1,
+        "name": "Nicholson Vazquez"
+      },
+      {
+        "id": 2,
+        "name": "Sheree Vega"
+      }
+    ],
+    "greeting": "Hello, Morgan! You have 7 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "5b40d23299a93a70694543ff",
+    "index": 7,
+    "guid": "ca0b3e74-704f-4191-860c-e0506c7e4c72",
+    "isActive": true,
+    "balance": "$1,195.28",
+    "age": 37,
+    "name": {
+      "first": "Janine",
+      "last": "Cash"
+    },
+    "company": "CABLAM",
+    "email": "janine.cash@cablam.org",
+    "phone": "+1 (915) 441-2509",
+    "address": "393 Prospect Avenue, Lund, Wyoming, 5830",
+    "registered": "Thursday, November 17, 2016 12:09 AM",
+    "tags": [
+      "fugiat",
+      "culpa",
+      "aliquip"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Chrystal Bender"
+      },
+      {
+        "id": 1,
+        "name": "Jacobs Dillard"
+      },
+      {
+        "id": 2,
+        "name": "Arnold Patton"
+      }
+    ],
+    "greeting": "Hello, Janine! You have 8 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "5b40d232614a4ba76d2d299a",
+    "index": 8,
+    "guid": "77830dae-dddb-42e9-a217-23a6f00a96fa",
+    "isActive": true,
+    "balance": "$1,472.80",
+    "age": 40,
+    "name": {
+      "first": "Everett",
+      "last": "Stone"
+    },
+    "company": "ENAUT",
+    "email": "everett.stone@enaut.io",
+    "phone": "+1 (997) 570-3121",
+    "address": "439 Ocean Parkway, Clarksburg, Nevada, 4995",
+    "registered": "Sunday, February 22, 2015 4:19 AM",
+    "tags": [
+      "excepteur",
+      "enim",
+      "laborum"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Wooten Elliott"
+      },
+      {
+        "id": 1,
+        "name": "Dalton Marks"
+      },
+      {
+        "id": 2,
+        "name": "Benson Burton"
+      }
+    ],
+    "greeting": "Hello, Everett! You have 9 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "5b40d232d5e79311517e15cf",
+    "index": 9,
+    "guid": "b2f31aff-b592-43d1-9624-fa121364d012",
+    "isActive": false,
+    "balance": "$2,548.77",
+    "age": 36,
+    "name": {
+      "first": "Marietta",
+      "last": "Schroeder"
+    },
+    "company": "ICOLOGY",
+    "email": "marietta.schroeder@icology.tv",
+    "phone": "+1 (903) 512-2858",
+    "address": "292 Woods Place, Sharon, Vermont, 2953",
+    "registered": "Friday, March 6, 2015 7:47 AM",
+    "tags": [
+      "ipsum",
+      "consectetur",
+      "do"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Ladonna Hogan"
+      },
+      {
+        "id": 1,
+        "name": "Bullock Reeves"
+      },
+      {
+        "id": 2,
+        "name": "Boyer Crosby"
+      }
+    ],
+    "greeting": "Hello, Marietta! You have 7 unread messages.",
+    "favoriteFruit": "banana"
+  }
+]
---
+++ dataset.js
@@ -0,0 +1,124 @@
+const data = require('./data')
+
+class DataSet {
+  constructor (array, caret = 0) {
+    this.data = array
+    this.caret = caret
+    this.selector = ''
+  }
+
+  exists (prop) {
+    const data = this.data.filter(el => prop in el)
+    return new DataSet(data)
+  }
+
+  has (prop) {
+    const data = this.data.filter(el => Array.isArray(el[prop]) ? el[prop].length : el[prop])
+    return new DataSet(data)
+  }
+
+  take (number = 1) {
+    return new DataSet(this.data.slice(this.caret, this.caret + number + 1), this.caret + number)
+  }
+
+  takeRandom (number = 1) {
+    if (number > this.data.length) number = this.data.length
+    const selected = randomTakes(this.data.length, number)
+    const data = this.data.filter((el, i) => selected.includes(i))
+    return new DataSet(data)
+  }
+
+  select (selector) {
+    this.selector = selector
+    return this
+  }
+
+  eq (value) {
+    if (!this.selector) return this
+    const data = this.data.filter(el => el[this.selector] === value)
+    return new DataSet(data)
+  }
+
+  gt (value) {
+    if (!this.selector) return this
+    const data = this.data.filter(el => el[this.selector] > value)
+    return new DataSet(data)
+  }
+
+  lt (value) {
+    if (!this.selector) return this
+    const data = this.data.filter(el => el[this.selector] < value)
+    return new DataSet(data)
+  }
+
+  gte (value) {
+    if (!this.selector) return this
+    const data = this.data.filter(el => el[this.selector] >= value)
+    return new DataSet(data)
+  }
+
+  lte (value) {
+    if (!this.selector) return this
+    const data = this.data.filter(el => el[this.selector] <= value)
+    return new DataSet(data)
+  }
+
+  asc (prop, type = 'string') {
+    if (this.data[this.caret] && this.data[this.caret][prop]) {
+      let data = []
+      switch (type) {
+        case 'num':
+        case 'number':
+          data = this.data.slice().sort((a, b) => a[prop] - b[prop])
+          break
+        case 'string':
+        case 'str':
+        default:
+          data = this.data.slice().sort((a, b) => a[prop].localeCompare(b[prop]))
+      }
+      return new DataSet(data)
+    }
+    return this
+  }
+
+  desc (prop, type = 'string') {
+    if (this.data[this.caret] && this.data[this.caret][prop]) {
+      let data = []
+      switch (type) {
+        case 'num':
+        case 'number':
+          data = this.data.slice().sort((a, b) => b[prop] - a[prop])
+          break
+        case 'string':
+        case 'str':
+        default:
+          data = this.data.slice().sort((a, b) => b[prop].localeCompare(a[prop]))
+      }
+      return new DataSet(data)
+    }
+    return this
+  }
+
+  sum (prop) {
+    return this.data.reduce((sum, el) => !isNaN(el[prop]) ? sum + el[prop] : sum, 0)
+  }
+
+  values () {
+    return this.data
+  }
+}
+
+function randomTakes (len, num, collection = []) {
+  const index = Math.floor(Math.random() * len)
+  if (!collection.includes(index)) collection.push(index)
+  return collection.length === num ? collection : randomTakes(len, num, collection)
+}
+
+const dataSet = new DataSet(data)
+const x = dataSet
+  .exists('company')
+  .has('index')
+  .select('age')
+  .gt(24)
+  .take(4)
+console.log(x)
