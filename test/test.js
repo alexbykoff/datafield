842f77d55b0948c394ad7d309c0077a4aa5aa3bb
Alex Bykov <spbeat@mail.ru>
2018-07-08 19:18:01 +0300

    add more tests

--- test/test.js
+++ test/test.js
@@ -27,19 +27,36 @@ describe('Suite 1', function () {
 
   describe('Nested props', function () {
     it('should find values for nested prop', function () {
-      const data = dataSet.has("name.first")
+      const data = dataSet.has('name.first')
       assert.equal(data.length(), 98)
     })
 
     it('should find nested prop', function () {
-      const data = dataSet.exists("name.first")
+      const data = dataSet.exists('name.first')
       assert.equal(data.length(), 99)
     })
 
     it('should remove all entries', function () {
-      const data = dataSet.exists("name.first.test")
+      const data = dataSet.exists('name.first.test')
       assert.equal(data.length(), 0)
     })
   })
 
+  describe('Sums', function () {
+    it('should sum age values from 100 entries', function () {
+      assert.equal(dataSet.sum('age'), 4317)
+    })
+
+    it('should sum age from selected sources', function () {
+      const data = dataSet.select('age').gt(45)
+      assert.equal(data.sum('age'), 2608)
+    })
+
+    it('should sum ages from 2 selections', function () {
+      const dataHigh = dataSet.select('age').gt(45)
+      const dataLow = dataSet.select('age').lte(45)
+      const sum = dataHigh.sum('age') + dataLow.sum('age')
+      assert.equal(sum, 4317)
+    })
+  })
 })
